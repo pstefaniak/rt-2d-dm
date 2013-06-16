e99f06082b67f2e4f5117a15074d5ee956a39bcc
Piotr Stefaniak <pstefaniak@atlassian.com>
2013-06-16 20:41:30 +0300

    dlaczego to jest w katalogu managers? i tym podobne?

--- WormsDeathmatch/WormsDeathmatch/WormsDeathmatch/Classes/Managers/Player.cs
+++ WormsDeathmatch/WormsDeathmatch/WormsDeathmatch/Classes/Managers/Player.cs
@@ -1,10 +1,10 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework;
-
+ 
 namespace WormsDeathmatch
 {
     /// <summary>
